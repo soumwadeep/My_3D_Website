7b2512c85873d1fde4401bb4aee295733f738fb8
Soumwadeep Guha <soumwadeep@gmail.com>
2021-04-24 11:23:35 +0530

    Update userinfo.php

--- userinfo.php
+++ userinfo.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$database = "u879917651_usermessages";
-$username = "u879917651_soumwadeep";
-$password = "Sdg@9753944206";
+$database = "enter_your_database's_name";
+$username = "enter_your_username";
+$password = "enter_your_password";
 
 $con =mysqli_connect($servername, $username, $password, $database);
 if ($con) {
